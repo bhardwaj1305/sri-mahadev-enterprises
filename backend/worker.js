2b188fe0aa6cd455978c60cc8a5fbcb13cfb2e00
Meesala Bhardwaj <uandme1305@gmail.com>
2026-08-29 14:50:30 +0530

    Update worker.js

--- backend/worker.js
+++ backend/worker.js
@@ -1,1 +1,22 @@
+import express from "express";
+import { httpServerHandler } from "cloudflare:node";
 
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Sri Mahadev Enterprises API is running",
+  });
+});
+
+app.get("/api/leads", (req, res) => {
+  res.json({
+    message: "Leads API is working",
+  });
+});
+
+app.listen(3000);
+
+export default httpServerHandler({ port: 3000 });
